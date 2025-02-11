cab31955740a1b504f2bd61d447d372fa4114e26
Elias Bennett <eliasthebennett@gmail.com>
2025-02-11 19:14:23 +0000

    Added a Card class with a constructor, getters, and toString()

---
+++ src/elias/snap/Card.java
@@ -0,0 +1,33 @@
+package elias.snap;
+
+public class Card {
+    private final String suit;
+    private final String symbol;
+    private final int value;
+
+    // Constructor
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    // Getters
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    // Override toString() for better readability
+    @Override
+    public String toString() {
+        return symbol + " of " + suit;
+    }
+}
