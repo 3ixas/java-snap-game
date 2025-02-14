ed3cad0d33a0c09d582d7de68d2ac83805151189
Elias Bennett <eliasthebennett@gmail.com>
2025-02-14 16:30:32 +0000

    Adding the constructor to the Snap class.

--- src/elias/snap/Snap.java
+++ src/elias/snap/Snap.java
@@ -1,4 +1,17 @@
 package elias.snap;
 
-public class Snap {
+import java.util.Scanner;
+
+public class Snap extends CardGame {
+    private Card previousCard;
+    private final Scanner scanner;
+
+    // Constructor
+    public Snap(String gameName) {
+        super(gameName); // Calls the constructor of CardGame
+        this.previousCard = null;
+        this.scanner = new Scanner(System.in);
+    }
+
+    
 }
