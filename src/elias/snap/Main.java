8c326aebbf47937e55febcfbf39cc235e296c6d7
Elias Bennett <eliasthebennett@gmail.com>
2025-02-15 18:00:43 +0000

    As I have already added a welcome message in the snap game, I removed the printGameInfo from the Main class so the welcome message is not duplicated.

--- src/elias/snap/Main.java
+++ src/elias/snap/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Snap snapGame = new Snap("Snap");
-        snapGame.printGameInfo();
         snapGame.playSnap();
     }
 }
